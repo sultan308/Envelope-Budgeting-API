824795c803447a3e073301cfc733f7f586cca481
Ahmed Soltan <ahmedsultan308@icloud.com>
2022-02-10 10:01:31 +0000

    Add app.js file

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
+const { Router } = require('express');
+
+const envelopeRouter = express.Router();
+
+envelopeRouter.get('/', (req,res,next) => {
+           
+});
+envelopeRouter.get('/:name', (req,res,next) => {
+
+});
+
+envelopeRouter.post('/', (req,res,next) => {
+
+});
+envelopeRouter.put('/', (req,res,next) => {
+
+});
+envelopeRouter.delete('/', (req,res,next) => {
+
+});
